48b1914db1fe050261321e0a61effaa520e1f5e1
lvhlily <lily.henry@gmail.com>
2022-03-12 18:48:33 -0500

    Contact and About Me pages

--- sandbox/js/toggle.js
+++ sandbox/js/toggle.js
@@ -9,4 +9,29 @@ document.querySelector(".nav-toggle").addEventListener("click", function(){
 
 
 
-// Interactive photos
+// Contact Form
+// document.querySelector(".btn").addEventListener("click", function(){
+
+//   e.preventDefault();
+
+//   const inputFname = document.querySelector("#inputFName").value;
+//   const inputLname = document.querySelector("#inputLName").value;
+
+//   if(inputFname.length <= 3) {
+//       messages.push("The first name is invalid");
+
+//   }
+
+//   if(inputLname.length <= 3) {
+//       messages.push("The last name is invalid");
+
+//   }
+// });
+
+
+
+
+document.querySelector(".btn").addEventListener("click", function(){
+alert("Thank you for submitting!");
+
+});
